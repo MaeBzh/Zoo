c91f7c3c6109472d5d4f23f5dc4709000eb09e2a
MaeBzh <maelenn.picaud@gmail.com>
2017-01-27 14:42:33 +0100

    Update index.php

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -70,7 +70,7 @@
 
             <div class="col-md-6">
                 <!-- DONUT CHART -->
-                <div class="box box-warning">
+                <div class="box box-primary">
                     <div class="box-header with-border">
                         <h3 class="box-title">Répartition des animaux par zone</h3>
                     </div>
