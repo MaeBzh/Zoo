178ef880adfc5dc5dcdddd09814d25c3c9359bb8
MaeBzh <maelenn.picaud@gmail.com>
2017-01-27 14:37:15 +0100

    Update afficher_liste_familles.php

--- application/view/animaux/afficher_liste_familles.php
+++ application/view/animaux/afficher_liste_familles.php
@@ -29,8 +29,6 @@
                                 <thead>
                                 <tr>
                                     <th>Désignation</th>
-                                    <th>Editer la fiche</th>
-                                    <th>Supprimer la fiche</th>
                                 </tr>
                                 </thead>
                                 <tbody>
@@ -38,51 +36,6 @@
                                     <tr>
                                         <!-- Famille -->
                                         <td><?php echo $famille->designation ?></td>
-                                        <td>
-                                            <a href="<?php echo URL ?>animaux/formulaire_edition_famille?id=<?php echo $famille->id ?>"
-                                               class="btn btn-warning btn-lg">
-                                                <i class="fa fa-pencil"></i>
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <button type="button" class="btn btn-warning btn-lg" data-toggle="modal"
-                                                    data-target="#zone_<?php echo $famille->id ?>">
-                                                <i class="fa fa-times"></i>
-                                            </button>
-
-                                            <div id="zone_<?php echo $famille->id ?>" class="modal fade" tabindex="-1"
-                                                 role="dialog">
-                                                <div class="modal-dialog" role="document">
-                                                    <div class="modal-content">
-                                                        <form role="form" method="POST"
-                                                              action="<?php echo URL ?>animaux/post_formulaire_suppression">
-                                                            <div class="modal-header">
-                                                                <button type="button" class="close"
-                                                                        data-dismiss="modal"
-                                                                        aria-label="Close"><span
-                                                                            aria-hidden="true">&times;</span>
-                                                                </button>
-                                                                <h4 class="modal-title">Suppression de la fiche famille</h4>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                                <input type="hidden" name="id"
-                                                                       value="<?php echo $famille->id ?>">
-                                                                <p>Etes-vous sûr de vouloir supprimer la
-                                                                    fiche <?php echo $famille->designation ?> ?</p>
-                                                            </div>
-                                                            <div class="modal-footer">
-                                                                <button type="button" class="btn btn-default"
-                                                                        data-dismiss="modal">Annuler
-                                                                </button>
-                                                                <button type="submit" class="btn btn-primary">
-                                                                    Supprimer
-                                                                </button>
-                                                            </div>
-                                                        </form>
-                                                    </div><!-- /.modal-content -->
-                                                </div><!-- /.modal-dialog -->
-                                            </div><!-- /.modal -->
-                                        </td>
                                     </tr>
                                     <?php }?>
                                  </tbody>
